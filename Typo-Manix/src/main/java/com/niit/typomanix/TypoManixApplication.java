21f9847d5930e9203acf8cdc88b80ea63dd4d59d
Training <gupta.jyoti1997@gmail.com>
2018-05-04 12:21:03 -0700

    First Commit

---
+++ Typo-Manix/src/main/java/com/niit/typomanix/TypoManixApplication.java
@@ -0,0 +1,12 @@
+package com.niit.typomanix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypoManixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypoManixApplication.class, args);
+	}
+}
